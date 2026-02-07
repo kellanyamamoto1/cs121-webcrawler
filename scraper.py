0f325833c4a6c06677834008672016fe40dd1fbc
kellanyamamoto1 <157436045+kellanyamamoto1@users.noreply.github.com>
2026-02-07 12:54:24 -0800

    creatd new functions to save format and save the analytics

--- scraper.py
+++ scraper.py
@@ -1,5 +1,59 @@
 import re
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urljoin, urldefrag, parse_qs
+from bs4 import BeautifulSoup
+from collections import Counter
+import json
+import os
+
+ANALYTICS_FILE = "analytics.json"
+def load_analytics():
+    if os.path.exists(ANALYTICS_FILE):
+        with open(ANALYTICS_FILE, 'r') as f:
+            data = json.load(f)
+            data['unique_pages'] = set(data.get('unique_pages', []))
+            data['all_words'] = Counter(data.get('all_words', {}))
+            data['subdomains'] = Counter(data.get('subdomains', {}))
+            return data
+    return {
+        'unique_pages': set(),
+        'word_counts': {},
+        'all_words': Counter(),
+        'subdomains': Counter(),
+        'longest_page': {'url': '', 'word_count': 0}
+    }
+
+def save_analytics(analytics):
+    data = {
+        'unique_pages': list(analytics['unique_pages']),
+        'word_counts': analytics['word_counts'],
+        'all_words': dict(analytics['all_words']),
+        'subdomains': dict(analytics['subdomains']),
+        'longest_page': analytics['longest_page']
+    }
+    with open(ANALYTICS_FILE, 'w') as f:
+        json.dump(data, f, indent=2)
+
+analytics = load_analytics()
+
+# Common English stop words
+STOP_WORDS = {
+    'a', 'about', 'above', 'after', 'again', 'against', 'all', 'am', 'an', 'and', 'any', 'are', 
+    'aren\'t', 'as', 'at', 'be', 'because', 'been', 'before', 'being', 'below', 'between', 'both',
+    'but', 'by', 'can\'t', 'cannot', 'could', 'couldn\'t', 'did', 'didn\'t', 'do', 'does', 'doesn\'t',
+    'doing', 'don\'t', 'down', 'during', 'each', 'few', 'for', 'from', 'further', 'had', 'hadn\'t',
+    'has', 'hasn\'t', 'have', 'haven\'t', 'having', 'he', 'he\'d', 'he\'ll', 'he\'s', 'her', 'here',
+    'here\'s', 'hers', 'herself', 'him', 'himself', 'his', 'how', 'how\'s', 'i', 'i\'d', 'i\'ll',
+    'i\'m', 'i\'ve', 'if', 'in', 'into', 'is', 'isn\'t', 'it', 'it\'s', 'its', 'itself', 'let\'s',
+    'me', 'more', 'most', 'mustn\'t', 'my', 'myself', 'no', 'nor', 'not', 'of', 'off', 'on', 'once',
+    'only', 'or', 'other', 'ought', 'our', 'ours', 'ourselves', 'out', 'over', 'own', 'same', 'shan\'t',
+    'she', 'she\'d', 'she\'ll', 'she\'s', 'should', 'shouldn\'t', 'so', 'some', 'such', 'than', 'that',
+    'that\'s', 'the', 'their', 'theirs', 'them', 'themselves', 'then', 'there', 'there\'s', 'these',
+    'they', 'they\'d', 'they\'ll', 'they\'re', 'they\'ve', 'this', 'those', 'through', 'to', 'too',
+    'under', 'until', 'up', 'very', 'was', 'wasn\'t', 'we', 'we\'d', 'we\'ll', 'we\'re', 'we\'ve',
+    'were', 'weren\'t', 'what', 'what\'s', 'when', 'when\'s', 'where', 'where\'s', 'which', 'while',
+    'who', 'who\'s', 'whom', 'why', 'why\'s', 'with', 'won\'t', 'would', 'wouldn\'t', 'you', 'you\'d',
+    'you\'ll', 'you\'re', 'you\'ve', 'your', 'yours', 'yourself', 'yourselves'
+}
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -16,6 +70,7 @@ def extract_next_links(url, resp):
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
     return list()
+
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
